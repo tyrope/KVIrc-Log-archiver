1fda7a8385d195ca87ff915080ba1b2575fb4aef
Tyrope <Tyrope@TyRope.nl>
2012-04-24 01:06:53 +0200

    Lots of moving curly brackets due to changed personal standard. New parameter, -i. check README for more information. replace file (or ignore with -i) instead of making a duplicate (issue #1) Added date check (issue #2 ) Move files instead of copy them ( issue #3 )
    Signed-off-by: Tyrope <Tyrope@TyRope.nl>

--- Program.cs
+++ Program.cs
@@ -4,93 +4,94 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
-namespace Archiver
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Archiver {
+    class Program {
+        static void checkWD(string wd, bool verbose = false) {
+            // This function checks if the directory exists, and if not creates it.
+            if(!Directory.Exists(wd)) {
+                if(verbose) { Console.WriteLine("Created Directory: " + wd); }
+                Directory.CreateDirectory(wd);
+            }
+        }
+        static void Main(string[] args) {
             /*
              * Testcase (for comments below)
              * Source file: D:\IRC_Logs\query_irma_weldon.freenode_2012.03.18.log
              * Output file: D:\IRC_Logs\freenode\2012\03\20\irma_weldon.log
             */
-            //check for launch parameters
-            bool verbose = false;
-            bool quiet = false;
-            string logFolder = "";
-            string archiveFolder = "";
-            for (int i = 0; i < args.Length; i++)
-            {
+
+            //Initialize variables
+            bool verbose = false, quiet = false, ignore = false;
+            string logFolder = "", archiveFolder = "";
+            int del = 0, move = 0, fail = 0, ignored = 0;
+
+            //apply parameters to variables.
+            for(int i = 0; i < args.Length; i++) {
                 int j = i + 1;
-                if (args[i] == "-a")
-                {
+                if(args[i] == "-a") {
                     archiveFolder = args[j];
                     i++;
-                }
-                else if (args[i] == "-l")
-                {
+                } else if(args[i] == "-l") {
                     logFolder = args[j];
                     i++;
-                }
-                else if (args[i] == "-v" || args[i] == "-verbose")
-                {
+                } else if(args[i] == "-v" || args[i] == "-verbose") {
                     verbose = true;
-                }
-                else if (args[i] == "-q" || args[i] == "-quiet")
-                {
+                } else if(args[i] == "-q" || args[i] == "-quiet") {
                     quiet = true;
+                } else if(args[i] == "-i" || args[i] == "-ignore") {
+                    ignore = true;
                 }
             }
-            if (archiveFolder.Length == 0)
-            {
+
+            //check for invalid and/or missing parameters
+            if(archiveFolder.Length == 0) {
                 Console.WriteLine("Archive folder parameter not given.");
                 Console.ReadLine();
                 return;
-            }
-            else if (!Directory.Exists(archiveFolder))
-            {
+            } else if(!Directory.Exists(archiveFolder)) {
                 Console.WriteLine("Archive folder doesn't exists, check the -a parameter.");
-                if (verbose) { Console.WriteLine("archiveFolder = " + archiveFolder); }
+                if(verbose) {
+                    Console.WriteLine("archiveFolder = " + archiveFolder);
+                }
                 Console.ReadLine();
                 return;
             }
 
-            if (logFolder.Length == 0)
-            {
+            if(logFolder.Length == 0) {
                 Console.WriteLine("Logging folder parameter not given.");
                 Console.ReadLine();
                 return;
-            }
-            else if (!Directory.Exists(logFolder))
-            {
+            } else if(!Directory.Exists(logFolder)) {
                 Console.WriteLine("Logging folder doesn't exists, check the -l parameter");
-                if (verbose) { Console.WriteLine("logFolder = " + logFolder); }
+                if(verbose) {
+                    Console.WriteLine("logFolder = " + logFolder);
+                }
                 Console.ReadLine();
                 return;
             }
-            if (verbose && quiet)
-            {
+
+            if(verbose && quiet) {
                 Console.WriteLine("Should I speak loud or shut up? Make up your mind!");
                 Console.ReadLine();
                 return;
             }
-            int del = 0, move = 0, fail = 0;
-            foreach (string file in Directory.GetFiles(logFolder))
-            {
+
+            ///main loop
+            foreach(string file in Directory.GetFiles(logFolder)) {
                 string[] filePath = file.Split('\\');
                 string fileName = filePath[filePath.Length - 1];
                 //channel_irma_weldon.freenode_2012.03.20.log
-                if (verbose) { Console.WriteLine("Parsing: " + fileName); }
-                if (fileName.StartsWith("deadchannel_"))
-                {
+                if(verbose) {
+                    Console.WriteLine("Parsing: " + fileName);
+                }
+                if(fileName.StartsWith("deadchannel_")) {
                     File.Delete(file);
-                    if (verbose) { Console.WriteLine("DEL: " + file); }
+                    if(verbose) {
+                        Console.WriteLine("Deleting deadchannel file: " + file);
+                    }
                     del++;
                     continue;
-                }
-                else if (fileName.StartsWith("query_") || fileName.StartsWith("channel_"))
-                {
+                } else if(fileName.StartsWith("query_") || fileName.StartsWith("channel_")) {
                     /*
                      * channel_irma_weldon.freenode 2012.03.20.log
                      * remove prefix -> irma_weldon.freenode_2012.03.18.log
@@ -103,50 +104,60 @@ namespace Archiver
                     string year = nameParts[1].Split('_')[1];
                     string month = nameParts[2];
                     string day = nameParts[3];
+                    string wd = archiveFolder+'\\'; //working directory
+                    DateTime now = DateTime.Parse(DateTime.Now.Day.ToString()+'-'+DateTime.Now.Month.ToString()+'-'+DateTime.Now.Year.ToString());
+                    DateTime logDate = DateTime.Parse(day+'-'+month+'-'+year);
 
+                    //Is this today's file? if so, ignore it.
+                    if(now == logDate) {
+                        ignored++;
+                        if(verbose) {
+                            Console.WriteLine("Ignoring file: " + file + "to: " + wd + '\\' + year+ '\\' + month + '\\' + day + '\\' + channel + ".txt");
+                        }
+                        continue;
+                    }
 
-                    channel = channel.Replace("%2d", "-").Replace("%2a", "[BNC]").Replace("%7b", "{").Replace("%7d", "}").Replace("%5b", "[").Replace("%5d", "]");
-                    if (!Directory.Exists(archiveFolder + '\\' + network))
-                    {
-                        if (verbose) { Console.WriteLine("Created Directory: " + archiveFolder + '\\' + network); }
-                        Directory.CreateDirectory(archiveFolder + '\\' + network);
-                    }
-                    if (!Directory.Exists(archiveFolder + '\\' + network + '\\' + year))
-                    {
-                        if (verbose) { Console.WriteLine("Created Directory: " + archiveFolder + '\\' + network + '\\' + year); }
-                        Directory.CreateDirectory(archiveFolder + '\\' + network + '\\' + year);
-                    }
-                    if (!Directory.Exists(archiveFolder + '\\' + network + '\\' + year + '\\' + month))
-                    {
-                        if (verbose) { Console.WriteLine("Created Directory: " + archiveFolder + '\\' + network + '\\' + year + '\\' + month); }
-                        Directory.CreateDirectory(archiveFolder + '\\' + network + '\\' + year + '\\' + month);
-                    }
-                    if (!Directory.Exists(archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day))
-                    {
-                        if (verbose) { Console.WriteLine("Created Directory: " + archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day); }
-                        Directory.CreateDirectory(archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day);
-                    }
+                    //replace characters to properly display their name
+                    channel = channel.Replace("%2d", "-").Replace("%2a", "[BNC]").Replace("%7b", "{").Replace("%7d", "}").Replace("%5b", "[").Replace("%5d", "]").Replace("%26", "&");
+
+                    //check if the working directory exists.
+                    wd += network;
+                    checkWD(wd, verbose);
+                    wd += '\\' + year;
+                    checkWD(wd, verbose);
+                    wd += '\\' + month;
+                    checkWD(wd, verbose);
+                    wd += '\\' + day;
+                    checkWD(wd, verbose);
                     //at this point the folder exists.
-                    if (File.Exists(archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day + '\\' + channel + ".txt"))
-                    {
-                        if (!quiet) { Console.WriteLine("File already exists: " + archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day + '\\' + channel + ".txt!"); }
+
+                    //does the file exist?
+                    if(File.Exists(wd + '\\' + channel + ".txt")) {
+                        if(!quiet) {
+                            Console.WriteLine("File already exists: " + wd + '\\' + channel + ".txt!");
+                        }
                         fail++;
-                    }
-                    else
-                    {
-                        //todo: see if year/month/day != today, if it does, ignore. if not, move.
-                        if (verbose) { Console.WriteLine("Copying file: " + file + "to: " + archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day + '\\' + channel + ".txt"); }
-                        move++;
-                        File.Copy(file, archiveFolder + '\\' + network + '\\' + year + '\\' + month + '\\' + day + '\\' + channel + ".txt");
+                    } else {
+                        try {
+                            File.Move(file, wd + '\\' + channel + ".txt");
+                            if(verbose) {
+                                Console.WriteLine("Moved file: " + file + "to: " + wd + '\\' + channel + ".txt");
+                            }
+                            move++;
+                        } catch {
+                            if(verbose) {
+                                Console.WriteLine("File couldn't be moved: " + file + "to: " + wd + '\\' + channel + ".txt");
+                            }
+                            fail++;
+                        }
                     }
                 }
             }
-            if (!quiet)
-            {
-                if (verbose)
-                {
+            if(!quiet) {
+                if(verbose) {
                     Console.WriteLine(del + " files Deleted (dead channel)");
                     Console.WriteLine(move + " files Archived successfully.");
+                    Console.WriteLine(ignored + " files Archived ignored.");
                     Console.WriteLine(fail + " files failed.");
                 }
                 Console.WriteLine("DONE! (press enter to exit.)");
